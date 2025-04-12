5d4e6b91afa5328fd6db6b182be8c5d6d54e65ef
Mariya Iushkova <mariesiushkova@gmail.com>
2025-04-12 08:46:26 +0300

    nearest search

--- src/app/pages/tour-item/nearest-tours/nearest-tours/nearest-tours.component.ts
+++ src/app/pages/tour-item/nearest-tours/nearest-tours/nearest-tours.component.ts
@@ -1,23 +1,37 @@
-import { Component, EventEmitter, inject, Input, model, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, inject, Input, model, OnChanges, 
+          OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { ITour } from '../../../../models/tours';
 import { ToursService } from '../../../../services/tours.service';
 import { GalleriaModule } from 'primeng/galleria';
 import { NgOptimizedImage } from '@angular/common';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-nearest-tours',
-  imports: [GalleriaModule, NgOptimizedImage],
+  imports: [GalleriaModule, 
+            NgOptimizedImage,
+            InputGroupModule,
+            InputGroupAddonModule,
+            ButtonModule,
+            InputTextModule
+          ],
   templateUrl: './nearest-tours.component.html',
   styleUrl: './nearest-tours.component.scss'
 })
-export class NearestToursComponent implements OnInit, OnChanges {
+export class NearestToursComponent implements OnInit, OnChanges, AfterViewInit {
   @Input()
   tourNearest: ITour = null;
 
   @Output() onTourChange = new EventEmitter<ITour>();
 
+  @ViewChild('searchInput') searchInput: ElementRef;
   tourService = inject(ToursService);
   toursArr = model<ITour[]>([]);
+  toursArrCopy = model<ITour[]>([]);
   activeLocationId: string;
   
   ngOnInit(): void {
@@ -32,8 +46,21 @@ export class NearestToursComponent implements OnInit, OnChanges {
       this.activeLocationId = tour?.locationId;
       this.tourService.getNearestTourByLocationId(this.activeLocationId).subscribe((data) => {
         this.toursArr.set(data);
+        this.toursArrCopy.set(data);
       });
     }
+  }
+
+  ngAfterViewInit(): void {
+    fromEvent<InputEvent>(this.searchInput.nativeElement, 'input').subscribe((ev) => {
+      const inputTargetValue = (ev.target as HTMLInputElement).value;
+      if (inputTargetValue === '') {
+        this.toursArr.set(this.toursArrCopy());
+      } else {
+        const newTours = this.tourService.searchTours(this.toursArrCopy(), inputTargetValue);
+        this.toursArr.set(newTours);
+      }
+    });
   }
 
   activeIndexChange(index: number) {
@@ -43,4 +70,6 @@ export class NearestToursComponent implements OnInit, OnChanges {
     
     this.onTourChange.emit(activeTour);
   }
+
+
 }
